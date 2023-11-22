69d3ebf7d796ba64349bb6f1884bd77e3eb892f0
DJSmith11 <142458119+DJSmith11@users.noreply.github.com>
2023-11-22 19:42:50 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,42 @@
+def main():
+    for item in data:
+import random
+        print(f"Random Number: {item}")
+
+if __name__ == "__main__":
+    data = generate_random_data()
+
+
+    return data
+def generate_random_data():
+    main()
+
+    data = [random.randint(1, 100) for _ in range(10)]    const data = [];
+    return data;
+
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+function generateRandomData() {
+    });
+main();
+
+}
+
+function main() {
+}    return data
+        print(f"Random Number: {item}")
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+def generate_random_data():
+
+def main():
+    data = generate_random_data()
+    main()
+
+import random
